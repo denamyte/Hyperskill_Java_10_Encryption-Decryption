981bb764152d7f7ebc085513216bf464c3e0dad8
denamyte <denamyte@gmail.com>
2021-03-25 02:34:23 +0200

    2021-03-25: Stage 1/6

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,6 +2,27 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String s = "we found a treasure!";
+        System.out.println(doEncrypt(simpleEncryptor, s));
     }
+
+    public static String doEncrypt(Encryptor encryptor, String s) {
+        return encryptor.encrypt(s);
+    }
+
+    interface Encryptor {
+        String encrypt(String s);
+    }
+
+    public static final Encryptor simpleEncryptor = str -> {
+        char low = 'a';
+        char hi = 'z';
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            char c1 = c >= low && c <= hi ? (char) (low + hi - c) : c;
+            sb.append(c1);
+        }
+        return sb.toString();
+    };
+
 }
