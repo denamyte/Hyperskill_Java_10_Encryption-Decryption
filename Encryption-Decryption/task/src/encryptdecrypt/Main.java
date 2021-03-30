4984677efea8d8e241e0fc60b8a81181f34b037f
denamyte <denamyte@gmail.com>
2021-03-31 02:55:07 +0300

    2021-03-31: Stage 4/6

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,27 +1,58 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String type = scanner.nextLine();
-        String s = scanner.nextLine();
-        int shift = Integer.parseInt(scanner.nextLine());
-        String processed = new EncDec3().performOperation(type, s, shift);
-        System.out.println(processed);
+        System.out.println(new EncDec4(args).getResult());
     }
 }
 
-class EncDec3 {
+class EncDec4 {
 
-    public String performOperation(String type, String s, int shift) {
-        return encDec(s, "enc".equals(type) ? shift : -shift);
+    static final String MODE = "-mode";
+    static final String KEY = "-key";
+    static final String DATA = "-data";
+    static final String ENC = "enc";
+
+    static final Map<String, String> defaultParams = Map.of(
+            MODE, ENC,
+            KEY, "0",
+            DATA, ""
+    );
+
+    private final Map<String, String> params;
+
+    public EncDec4(String[] args) {
+        params = parseCLIParams(args);
     }
 
-    private String encDec(String s, int shift) {
+    public static Map<String, String> parseCLIParams(String[] args) {
+        int length = args.length;
+        int taken = 0;
+        Map<String, String> params = new HashMap<>();
+        while (taken < length) {
+            params.put(args[taken++], args[taken++]);
+        }
+        return params;
+    }
+
+    private String getParam(String key) {
+        return params.getOrDefault(key, defaultParams.get(key));
+    }
+
+    public String getResult() {
+        return performOperation(getParam(MODE), getParam(DATA), Integer.parseInt(getParam(KEY)));
+    }
+
+    public static String performOperation(String mode, String s, int shift) {
+        return encDec(s, ENC.equals(mode) ? shift : -shift);
+    }
+
+    private static String encDec(String s, int shift) {
         return s.chars().map(i -> i + shift)
                 .mapToObj(i -> (char) i)
                 .map(String::valueOf)
